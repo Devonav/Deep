f115704179a0cf012ac30a68e372f36fe0263ee4
Devon Villalona <de167342@ucf.edu>
2025-07-03 00:32:36 -0400

    Add .env support and update dependencies in server application

--- go-trigger-app/server/server.go
+++ go-trigger-app/server/server.go
@@ -1,33 +1,34 @@
-// server.go
+// server/server.go
 package main
 
 import (
 	"database/sql"
-	"go-trigger-app/db"      // Your existing db package
-	"go-trigger-app/storage" // <-- Import the new storage package
+	"go-trigger-app/db"
+	"go-trigger-app/storage"
 	"go-trigger-app/web"
 	"log"
 	"os"
 
+	"github.com/joho/godotenv" // <-- Import godotenv
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	// Use the DATABASE_URL environment variable directly
+	// Load .env file. It will not override variables that are already set.
+	// This means on the server, the .profile variables will be used.
+	// On your Mac, this will load your local .env file.
+	godotenv.Load() // <-- Add this line
+
 	d, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer d.Close()
 
-	// Initialize Wasabi Session
-	storage.InitS3() // <-- Add this line
+	storage.InitS3()
 
-	// CORS is enabled only in prod profile
 	cors := os.Getenv("profile") == "prod"
 	app := web.NewApp(db.NewDB(d), cors)
 	err = app.Serve()
 	log.Println("Error", err)
 }
-
-// You can now DELETE the entire dataSource() function.
